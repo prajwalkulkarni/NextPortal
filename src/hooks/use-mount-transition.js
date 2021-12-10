5355be690356c5b70a2923503dc27c187b72c0b8
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-12-10 23:17:40 +0530

    Apply transition on mount and unmount - custom hook

---
+++ src/hooks/use-mount-transition.js
@@ -0,0 +1,24 @@
+import {useEffect, useState} from 'react'
+export default function useTransitionOnMount(hasMounted,unmountDelay){
+      
+  const [hasTransitionedIn,setTransition] = useState(false)
+  // console.log("Use transiton on mount triggered")
+  useEffect(()=>{
+    let timeout 
+    // console.log("Use effect triggered") 
+    if(hasMounted && !hasTransitionedIn){
+      
+      setTransition(true)
+    }
+    else if(!hasMounted && hasTransitionedIn){
+      timeout = setTimeout(()=>{
+        // console.log("Triggered after:"+unmountDelay+"seconds")
+        setTransition(false)
+      },unmountDelay)
+    }
+
+    return ()=>clearTimeout(timeout)
+
+  },[hasTransitionedIn,hasMounted,unmountDelay])
+  return hasTransitionedIn
+}
