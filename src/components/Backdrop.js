8243f4b48af6c6bfe676f96c67833e8c35d586fc
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2024-02-09 22:51:24 +0530

    Update Backdrop.js

--- src/components/Backdrop.js
+++ src/components/Backdrop.js
@@ -2,10 +2,7 @@ import ReactDOM from 'react-dom'
 import styles from './Backdrop.module.css'
 import {usePortal} from '../hooks/use-portal'
 export default function Backdrop(props){
-
     const rootContainer = usePortal("backdrop",props.isMounted,false)
-
-    // const [rootContainer,containerRef] = usePortal("backdrop")
 
     return rootContainer?ReactDOM.createPortal(<div onClick={props.onClick} className={styles['backdrop']}/>,rootContainer)
     :null
