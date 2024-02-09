12cff6b1cc6e7b093aff44d26307cd758c7553d3
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2024-02-09 22:49:38 +0530

    Update use-portal.js

--- src/hooks/use-portal.js
+++ src/hooks/use-portal.js
@@ -1,52 +1,21 @@
 import {useEffect, useState} from 'react'
 
-// export const usePortal = (id) => {
-//     const containerRef = useRef()
-//     const element = document.getElementById(id)
-    
-//     const parentElement = document.querySelector("#__next")
-//     let rootContainer
-//     if(element){
-//         rootContainer = element
-//     }
-//     else{
-//         rootContainer = document.createElement("div")
-//         rootContainer.setAttribute("id",id)
-//     }
-
-//     parentElement?.insertAdjacentElement("afterend",rootContainer)
-//     containerRef = rootContainer
-
-//     return [rootContainer,containerRef]
-// }
-
-
-export const usePortal = (id,isMounted,isTransitioned) => {
-    // var containerRef = useRef()
-    
+export const usePortal = (id,isMounted,isTransitioned) => {    
     // let parentElement
     let element
     const [rootContainer,setRootContainer] = useState(null)
     useEffect(()=>{
         element = document.getElementById(id)
-        
-        
+    
         if(element){
-            setRootContainer(element)
-            
+            setRootContainer(element)        
         }
         else{
             element = document.createElement("div")
             element.setAttribute("id",id)
             setRootContainer(element)
-           
         }
-
-        
-        
-        // console.log("use effect called")
     },[])
-
 
     if(rootContainer&&isMounted&&!isTransitioned){
         const parentElement = document.querySelector("#__next")
@@ -56,13 +25,5 @@ export const usePortal = (id,isMounted,isTransitioned) => {
         return rootContainer
     }
 
-
-    // containerRef = rootContainer
-    // console.log(rootContainer)
-
     return rootContainer
-    
-    // console.log(containerRef)
-    
-    
 }
